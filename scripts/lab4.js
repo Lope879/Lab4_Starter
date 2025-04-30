a4080801a884d40832ca0055851fb8dbabad115c
Cesar Lopez <100965607+Lope879@users.noreply.github.com>
2025-04-29 23:13:22 -0700

    Undid mistake, wrong branch

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -29,10 +29,6 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
-    if (prices.length == 0 || Array.isArray(prices) == false 
-        || discount > 1 || discount < 0 || typeof discount != "number") {
-        return false;
-    }
     const discounted = []
     const length = prices.length;
     let discountedPrice = 0
